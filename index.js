253b0cd512b85a3475c21607ba32b9ed837021d1
unknown <nduatip72@gmail.com>
2023-05-22 20:11:29 +0300

    lastone

--- index.js
+++ index.js
@@ -10,17 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 
 //routes
-app.use("/api/v1/patients", require("./routes/patients.js"));
-app.use("/api/v1/orders", require("./routes/orders.js"));
-app.use("/api/v1/clinicians", require("./routes/clinicians.js"));
-app.use("/api/v1/couriers", require("./routes/couriers.js"));
-app.use("/api/v1/facilities", require("./routes/facilities.js"));
-app.use("/api/v1/ussd", require("./routes/ussd.js"));
+app.use("/api/v1/patients", require("./routes/patients"));
+app.use("/api/v1/orders", require("./routes/orders"));
+app.use("/api/v1/clinicians", require("./routes/clinicians"));
+app.use("/api/v1/couriers", require("./routes/couriers"));
+app.use("/api/v1/facilities", require("./routes/facilities"));
+app.use("/api/v1/ussd", require("./routes/ussd"));
 
 app.get("/", (req, res) => {
   res.send("Server is running.");
 });
-
 
 app.listen(port, () => {
   DbConnect();
